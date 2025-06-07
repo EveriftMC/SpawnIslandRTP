9d9ee23287819c4c3eb2c560fa57040fe9cbfb17
Strokkur424 <strokkur.24@gmail.com>
2025-06-07 20:11:39 +0200

    Add plugin logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,23 +9,27 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://eldonexus.de/repository/maven-public/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper", "paper-api", "1.21.5-R0.1-SNAPSHOT")
+    compileOnly("net.strokkur", "betterrtp", "3.6.13") // <-- Yes, this is a manual upload because their repo went down
 }
 
 tasks {
     runServer {
         minecraftVersion("1.21.5")
+        
+        // BetterRTP plugin
+        downloadPlugins.hangar("BetterRTP", "3.6.8")
+        
         jvmArgs("-Xmx2G", "-Xms2G", "-Dcom.mojang.eula.agree=true")
     }
 
     processResources {
         filesMatching("paper-plugin.yml") {
-            expand(
-                "version" to version
-            )
+            expand("version" to version)
         }
     }
 }
--- src/main/java/net/strokkur/spawnislandrtp/SpawnIslandRTPPlugin.java
+++ src/main/java/net/strokkur/spawnislandrtp/SpawnIslandRTPPlugin.java
@@ -1,11 +1,79 @@
 package net.strokkur.spawnislandrtp;
 
+import com.mojang.brigadier.Command;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import me.SuperRonanCraft.BetterRTP.player.rtp.RTP_TYPE;
+import me.SuperRonanCraft.BetterRTP.references.helpers.HelperRTP;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
+import java.util.Optional;
+
+@SuppressWarnings("UnstableApiUsage")
 @NullMarked
-public final class SpawnIslandRTPPlugin extends JavaPlugin {
-    
-    // ...
-    
+public final class SpawnIslandRTPPlugin extends JavaPlugin implements Listener {
+
+    private int minY = 0;
+    private @Nullable World world;
+
+    @Override
+    public void onLoad() {
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS.newHandler(
+            event -> event.registrar().register(Commands.literal("spawnislandrtp")
+                .requires(stack -> stack.getSender().hasPermission("spawnislandrtp.command"))
+                .then(Commands.literal("reload")
+                    .executes(ctx -> {
+                        reload();
+                        ctx.getSource().getSender().sendRichMessage("<green>Successfully reloaded SpawnIslandRTP.");
+                        return Command.SINGLE_SUCCESS;
+                    })
+                )
+                .build()
+            )
+        ));
+    }
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        reload();
+    }
+
+    void reload() {
+        this.saveDefaultConfig();
+        minY = this.getConfig().getInt("min-y");
+
+        String worldName = this.getConfig().getString("world");
+        Optional<World> world = Optional.ofNullable(worldName)
+            .map(Bukkit::getWorld);
+        if (world.isEmpty()) {
+            this.getComponentLogger().warn("Failed to find world for {}. Please define a valid world!", worldName);
+        } else {
+            this.world = world.get();
+        }
+    }
+
+    @EventHandler
+    void onFallBelowMinY(PlayerMoveEvent event) {
+        if (this.world == null) {
+            return;
+        }
+
+        if (this.world != event.getTo().getWorld()) {
+            return;
+        }
+
+        if (event.getTo().y() > this.minY) {
+            return;
+        }
+
+        HelperRTP.tp(event.getPlayer(), this.world, null, RTP_TYPE.ADDON);
+    }
 }

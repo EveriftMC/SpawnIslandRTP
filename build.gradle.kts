256903a2d09bf8bf01a8d372eb048661a2d6db4f
Strokkur24 <strokkur.24@gmail.com>
2025-09-25 15:58:23 +0200

    chore: update indent code style

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,35 @@
 plugins {
-    id("java")
-    id("xyz.jpenilla.run-paper") version "2.3.1"
+  id("java")
+  id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 group = "net.strokkur"
-version = "1.0-SNAPSHOT"
+version = "1.0.1"
 
 repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://eldonexus.de/repository/maven-public/")
+  mavenCentral()
+  maven("https://repo.papermc.io/repository/maven-public/")
+  maven("https://eldonexus.de/repository/maven-public/")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper", "paper-api", "1.21.5-R0.1-SNAPSHOT")
-    compileOnly("net.strokkur", "betterrtp", "3.6.13") // <-- Yes, this is a manual upload because their repo went down
+  compileOnly("io.papermc.paper", "paper-api", "1.21.5-R0.1-SNAPSHOT")
+  compileOnly("net.strokkur", "betterrtp", "3.6.13") // <-- Yes, this is a manual upload because their repo went down
 }
 
 tasks {
-    runServer {
-        minecraftVersion("1.21.5")
-        
-        // BetterRTP plugin
-        downloadPlugins.hangar("BetterRTP", "3.6.8")
-        
-        jvmArgs("-Xmx2G", "-Xms2G", "-Dcom.mojang.eula.agree=true")
-    }
+  runServer {
+    minecraftVersion("1.21.5")
 
-    processResources {
-        filesMatching("paper-plugin.yml") {
-            expand("version" to version)
-        }
+    // BetterRTP plugin
+    downloadPlugins.hangar("BetterRTP", "3.6.8")
+
+    jvmArgs("-Xmx2G", "-Xms2G", "-Dcom.mojang.eula.agree=true")
+  }
+
+  processResources {
+    filesMatching("paper-plugin.yml") {
+      expand("version" to version)
     }
+  }
 }
--- src/main/java/net/strokkur/spawnislandrtp/SpawnIslandRTPPlugin.java
+++ src/main/java/net/strokkur/spawnislandrtp/SpawnIslandRTPPlugin.java
@@ -28,82 +28,82 @@ import java.util.Optional;
 @NullMarked
 public final class SpawnIslandRTPPlugin extends JavaPlugin implements Listener {
 
-    private int minY = 0;
-    private @Nullable World world;
+  private int minY = 0;
+  private @Nullable World world;
 
-    @Override
-    public void onLoad() {
-        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS.newHandler(
-            event -> event.registrar().register(Commands.literal("spawnislandrtp")
-                .requires(stack -> stack.getSender().hasPermission("spawnislandrtp.command"))
-                .then(Commands.literal("reload")
-                    .executes(ctx -> {
-                        reload();
-                        ctx.getSource().getSender().sendRichMessage("<green>Successfully reloaded SpawnIslandRTP.");
-                        return Command.SINGLE_SUCCESS;
-                    })
-                )
-                .build()
+  @Override
+  public void onLoad() {
+    this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS.newHandler(
+        event -> event.registrar().register(Commands.literal("spawnislandrtp")
+            .requires(stack -> stack.getSender().hasPermission("spawnislandrtp.command"))
+            .then(Commands.literal("reload")
+                .executes(ctx -> {
+                  reload();
+                  ctx.getSource().getSender().sendRichMessage("<green>Successfully reloaded SpawnIslandRTP.");
+                  return Command.SINGLE_SUCCESS;
+                })
             )
-        ));
+            .build()
+        )
+    ));
+  }
+
+  @Override
+  public void onEnable() {
+    Bukkit.getPluginManager().registerEvents(this, this);
+    reload();
+  }
+
+  void reload() {
+    this.saveDefaultConfig();
+    minY = this.getConfig().getInt("min-y");
+
+    String worldName = this.getConfig().getString("world");
+    Optional<World> world = Optional.ofNullable(worldName)
+        .map(NamespacedKey::fromString)
+        .map(Bukkit::getWorld);
+
+    if (world.isEmpty()) {
+      this.getComponentLogger().warn("Failed to find world for {}. Please define a valid world!", worldName);
+    } else {
+      this.world = world.get();
     }
 
-    @Override
-    public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
-        reload();
+    String finalWorldName = this.world == null ? "null" : this.world.getName();
+    this.getComponentLogger().info("Successfully loaded world {} and min-y {}.", finalWorldName, minY);
+  }
+
+  @EventHandler
+  void onFallBelowMinY(PlayerMoveEvent event) {
+    if (this.world == null) {
+      return;
     }
 
-    void reload() {
-        this.saveDefaultConfig();
-        minY = this.getConfig().getInt("min-y");
-
-        String worldName = this.getConfig().getString("world");
-        Optional<World> world = Optional.ofNullable(worldName)
-            .map(NamespacedKey::fromString)
-            .map(Bukkit::getWorld);
-
-        if (world.isEmpty()) {
-            this.getComponentLogger().warn("Failed to find world for {}. Please define a valid world!", worldName);
-        } else {
-            this.world = world.get();
-        }
-
-        String finalWorldName = this.world == null ? "null" : this.world.getName();
-        this.getComponentLogger().info("Successfully loaded world {} and min-y {}.", finalWorldName, minY);
+    if (this.world != event.getTo().getWorld()) {
+      return;
     }
 
-    @EventHandler
-    void onFallBelowMinY(PlayerMoveEvent event) {
-        if (this.world == null) {
-            return;
-        }
-
-        if (this.world != event.getTo().getWorld()) {
-            return;
-        }
-
-        if (event.getTo().y() > this.minY) {
-            return;
-        }
-        
-        // This logic is yanked from HelperRTP.tp because I do not want it to send any error messages.
-        Player player = event.getPlayer();
-        
-        boolean ignoreCooldown = true;
-        boolean ignoreDelay = true;
-
-        RTP_PlayerInfo playerInfo = new RTP_PlayerInfo(!ignoreDelay, true, !ignoreCooldown);
-        World world = HelperRTP.getActualWorld(player, this.world, null);
-        RTPSetupInformation setup_info = new RTPSetupInformation(world, player, player, true, null, RTP_TYPE.FORCED, null, playerInfo);
-
-        WorldPlayer pWorld = HelperRTP.getPlayerWorld(setup_info);
-        RTP_ERROR_REQUEST_REASON cantReason = HelperRTP_Check.canRTP(player, player, pWorld, setup_info.getPlayerInfo());
-
-        if (cantReason != null) {
-            return;
-        }
-
-        BetterRTP.getInstance().getRTP().start(pWorld);
+    if (event.getTo().y() > this.minY) {
+      return;
     }
+
+    // This logic is yanked from HelperRTP.tp because I do not want it to send any error messages.
+    Player player = event.getPlayer();
+
+    boolean ignoreCooldown = true;
+    boolean ignoreDelay = true;
+
+    RTP_PlayerInfo playerInfo = new RTP_PlayerInfo(!ignoreDelay, true, !ignoreCooldown);
+    World world = HelperRTP.getActualWorld(player, this.world, null);
+    RTPSetupInformation setup_info = new RTPSetupInformation(world, player, player, true, null, RTP_TYPE.FORCED, null, playerInfo);
+
+    WorldPlayer pWorld = HelperRTP.getPlayerWorld(setup_info);
+    RTP_ERROR_REQUEST_REASON cantReason = HelperRTP_Check.canRTP(player, player, pWorld, setup_info.getPlayerInfo());
+
+    if (cantReason != null) {
+      return;
+    }
+
+    BetterRTP.getInstance().getRTP().start(pWorld);
+  }
 }
